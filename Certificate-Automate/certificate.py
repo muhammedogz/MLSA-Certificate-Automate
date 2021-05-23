efeb69cb35ac1bb7d319c737ad1688617373112f
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-05-23 20:40:21 +0300

    Add gitignore1

--- Certificate-Automate/certificate.py
+++ Certificate-Automate/certificate.py
@@ -5,3 +5,12 @@ from docx import Document
 
 print("hello world")
 
+
+filename = "Certificate-Automate/Event Certificate Template.docx"
+f = open(filename, 'rb')
+doc = Document(f)
+
+for section in doc.sections:
+    header = section.header
+    for paragraph in header.paragraphs:
+        print(paragraph.text)
