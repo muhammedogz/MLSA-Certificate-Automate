c44a2e1e9381cad0cffed98c47c620c33d23ff2e
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-05-26 05:39:55 +0300

    Update requirements
    Complete method that converts docs to pdf

--- main_certificate.py
+++ main_certificate.py
@@ -3,29 +3,41 @@
 import os
 from certificate import *
 from docx import Document
+from docx2pdf import convert
 
 
 # create output folder if not exist
 try:
-    os.mkdir("Output")
+    os.makedirs("Output/Doc")
+    os.makedirs("Output/PDF")
 except OSError:
     pass
 
-def create_docx_files(doc, dict_participant, event, ambassador):
-    replace_participant_name(doc, dict_participant)
-    replace_event_name(doc, event)
-    replace_ambassador_name(doc, ambassador)
-    doc.save('Output/{}.docx'.format(dict_participant))
+def create_docx_files(filename, list_participate, event, ambassador):
+
+
+    for participate in list_participate:
+        # use original file everytime
+        doc = Document(filename)
+
+        replace_participant_name(doc, participate)
+        replace_event_name(doc, event)
+        replace_ambassador_name(doc, ambassador)
+        doc.save('Output/Doc/{}.docx'.format(participate))
+        convert('Output/Doc/{}.docx'.format(participate), 'Output/Pdf/{}.pdf'.format(participate))
+        for table in doc.tables:
+            for row in table.rows:
+                for cell in row.cells:
+                    for p in cell.paragraphs:
+                        print(p.text)
+
+    
 
 filename = "Certificate Template/Event Certificate Template.docx"
-doc = Document(filename) 
 
-create_docx_files(doc, "Domates Patates", "WSL and VSCode ha", "Aysem Yas")
+list_participate = ["Muhammed Oğuz", "Ayşegül Aydoğan", "Joe Biden", "Barack Obama"]
 
-for table in doc.tables:
-    for row in table.rows:
-        for cell in row.cells:
-            for p in cell.paragraphs:
-                print(p.text)
+create_docx_files(filename, list_participate, "WSL and VSCode ha", "Aysem Yas")
+
 
 
