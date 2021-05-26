c69cd295733083173520eb45b74b0f32b0b6ef87
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-05-26 04:05:43 +0300

    Modify gitignore
    Modify certificate doc to one line
    Add replace name method

--- Certificate-Automate/certificate.py
+++ Certificate-Automate/certificate.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
-
+import re
 from docx import Document
 
+def docx_replace_regex(doc_obj, regex , replace):
 
-print("hello world")
+    for p in doc_obj.paragraphs:
+        if regex.search(p.text):
+            print(p.text)
+            inline = p.runs
+            # Loop added to work with runs (strings with same style)
+            for i in range(len(inline)):
+                if regex.search(inline[i].text):
+                    text = regex.sub(replace, inline[i].text)
+                    inline[i].text = text
 
+    for table in doc_obj.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                docx_replace_regex(cell, regex , replace)
+
+
+def replace_participant_name(doc, name):
+    string = "Samplefirstname Samplelastname"
+    reg = re.compile(r""+string)
+    replace = r""+name
+    docx_replace_regex(doc, reg , replace)
 
 filename = "Certificate-Automate/Event Certificate Template.docx"
-f = open(filename, 'rb')
-doc = Document(f)
+doc = Document(filename)
 
-for section in doc.sections:
-    header = section.header
-    for paragraph in header.paragraphs:
-        print(paragraph.text)
+replace_participant_name(doc, "Muhammed Oğuz")
+
+doc.save('Output/result1.docx')
